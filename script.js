ff38552746dfda384ff647ee709c169bfe1a1fe7
clownoftheweekcode <clownofthe_week.code@outlook.com>
2026-07-08 20:40:40 +0000

    fixed the issue with the timer window not closing or able to be dragged

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ setInterval(function() {
 
 // make window draggable //
 dragElement(document.getElementById("welcome"));
+dragElement(document.getElementById("timer"));
 function dragElement(el) {
     var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
     if (document.getElementById(el.id + "header")) {
@@ -38,27 +39,35 @@ function dragElement(el) {
 
 // opening and closing welcome window //
 var welcomeScreen = document.querySelector("#welcome");
+var timerScreen = document.querySelector("#timer");
 function closeWindow(el) {
     // el.classList.remove("active");
     el.style.display = "none";
 }
 function openWindow(el) {
-    if (!el.classList.contains("active")) {
+    // if (!el.classList.contains("active")) {
         // el.classList.add("active");
-        el.style.left = 300 + "px";
-        el.style.top = 300 + "px";
-    }
+        // el.style.left = 300 + "px";
+        // el.style.top = 300 + "px";
+    // }
     el.style.display = "block";
     biggestIndex++;
     el.style.zIndex = biggestIndex;
 }
 var welcomeScreenClose = document.querySelector("#welcomeclose"), welcomeScreenOpen = document.querySelector("#welcomeopen");
+var timerScreenClose = document.querySelector("#timerclose");
+var timerScreenOpen = document.querySelector("#timeropen");
 welcomeScreenClose.addEventListener("click", function() {
     closeWindow(welcomeScreen);
 });
 welcomeScreenOpen.addEventListener("click", function() {
-    openWindow(welcomeScreen);
+     openWindow(welcomeScreen);
 });
+timerScreenOpen.addEventListener("click", () => 
+ openWindow(timerScreen));
+
+timerScreenClose.addEventListener("click", () => 
+ closeWindow(timerScreen));
 // open app //
 var selectedIcon = undefined;
 function selectIcon(el) {
@@ -88,6 +97,7 @@ var appOpen = document.querySelector("#appopen");
 appScreenClose.addEventListener("click", () => 
     closeWindow(appScreen));
 
+
 function addWindowTapHandling(el) {
     el.addEventListener("mousedown", () =>
     handleIconTap(el))
@@ -96,6 +106,7 @@ appOpen.addEventListener("click", () =>
     openWindow(appScreen));
 addWindowTapHandling(welcomeScreen);
 addWindowTapHandling(appScreen);
+addWindowTapHandling(timerScreen);
 var biggestIndex = 1;
 function initializeIcon(name) {
     var icon = document.querySelector('#' + name + 'Icon');
@@ -113,5 +124,4 @@ function initializeWindow(elName) {
     }
 }
 initializeWindow("welcome");
-initializeWindow("timer");
 initializeWindow("app");
